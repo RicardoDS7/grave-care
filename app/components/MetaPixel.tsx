f54fd09157b47dc620e2770f3223e67d3c014b0b
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-24 15:46:36 +0200

    Facebook Pixel

--- app/components/MetaPixel.tsx
+++ app/components/MetaPixel.tsx
@@ -4,45 +4,40 @@ import { useEffect } from 'react';
 
 declare global {
   interface Window {
-    fbq: FbqFunction;
-    _fbq?: FbqFunction;
+    fbq?: (...args: any[]) => void;
+    _fbq?: unknown;
   }
 }
-
-type FbqFunction = {
-  (...args: unknown[]): void;
-  callMethod?: (...args: unknown[]) => void;
-  queue?: unknown[];
-  loaded?: boolean;
-  version?: string;
-  push?: (...args: unknown[]) => void;
-};
 
 export default function MetaPixel() {
   useEffect(() => {
     if (typeof window === 'undefined') return;
-    if (window.fbq?.loaded) return;
+    if (window.fbq) return;
 
-    // Inject the Meta Pixel script
-    (function (f: Partial<Window>, b: Document, e: string, v: string, n?: FbqFunction, t?: HTMLScriptElement, s?: Node) {
-      if (typeof f.fbq !== 'undefined') return;
-      n = function (...args: unknown[]) {
-        (n!.callMethod ? n!.callMethod : n!.queue!.push).apply(n, args);
-      } as FbqFunction;
+    (function (f: any, b: Document, e: string, v: string) {
+      const n: any = function (...args: unknown[]) {
+        if (n.callMethod) {
+          n.callMethod(...args);
+        } else {
+          n.queue.push(args);
+        }
+      };
+      if (!f._fbq) f._fbq = n;
       n.push = n;
       n.loaded = true;
       n.version = '2.0';
       n.queue = [];
-      (f as Window).fbq = n;
-      t = b.createElement(e) as HTMLScriptElement;
+      f.fbq = n;
+
+      const t = b.createElement(e) as HTMLScriptElement; // Cast to HTMLScriptElement
       t.async = true;
       t.src = v;
-      s = b.getElementsByTagName(e)[0];
+      const s = b.getElementsByTagName(e)[0];
       s?.parentNode?.insertBefore(t, s);
     })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
-    window.fbq('init', '717660720718297');
-    window.fbq('track', 'PageView');
+    (window.fbq as ((...args: any[]) => void) | undefined)?.('init', '717660720718297');
+    (window.fbq as ((...args: any[]) => void) | undefined)?.('track', 'PageView');
   }, []);
 
   return (
