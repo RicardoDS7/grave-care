bb15bc37d8a6dfc495f0cd4be246be0117891343
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-10-16 15:23:56 +0200

    Add local SEO

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ import Footer from "./components/Footer";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 import { trackViewContent } from "./utils/fbpixel";
-// import FloatingWhatsAppButton from "./components/WhatsAppFloatingButton";
+import Script from "next/script";
+import FloatingWhatsAppButton from "./components/WhatsAppFloatingButton";
 
 // const TestimonialsCarousel = dynamic(() => import("./components/Testimonials"), {ssr: false});
 
@@ -27,6 +28,27 @@ export default function Home() {
   useEffect(() => {
     trackViewContent();
   }, []);
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    "serviceType": "Grave Maintenance and Cleaning",
+    "provider": {
+      "@type": "Organization",
+      "name": "GraveCare",
+    },
+    "areaServed": [
+      { "@type": "Place", "name": "West Park Cemetery, Johannesburg, South Africa" },
+      { "@type": "Place", "name": "Zuurfontein Cemetery, Kempton Park, South Africa" },
+      { "@type": "Place", "name": "Heidelberg Cemetery, Heidelberg, South Africa" },
+      { "@type": "Place", "name": "Boksburg Cemetery, Boksburg, South Africa" },
+      { "@type": "Place", "name": "Edenvale Cemetery, Edenvale, South Africa" },
+      { "@type": "Place", "name": "Germiston Cemetery, Germiston, South Africa" },
+      { "@type": "Place", "name": "Benoni Cemetery, Benoni, South Africa" },
+      { "@type": "Place", "name": "Alberton Cemetery, Alberton, South Africa" },
+    ],
+    "url": "https://gravecare.co.za/",
+  };
 
   return (
     <main className="min-h-screen bg-brand-background text-brand-foreground font-sans">
@@ -49,10 +71,16 @@ export default function Home() {
       
       <GetStartedForm />
 
-      {/* <FloatingWhatsAppButton /> */}
+      <FloatingWhatsAppButton />
 
       <Footer />
 
+      <Script
+        id="gravecare-jsonld"
+        type="application/ld+json"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
 
     </main>
   );
