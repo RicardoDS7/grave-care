3ac6f0a851d75b7f34c22f834624f0b98108de4c
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-12-01 21:08:08 +0200

    Fix form and updates

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -20,7 +20,7 @@ export default function HeroSection({
   altText
 }: HeroSectionProps) {
   return (
-    <section className="relative h-[95vh] flex items-center justify-center overflow-hidden">
+    <section className="relative flex flex-col h-[80vh] md:h-[90vh] flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
@@ -96,7 +96,7 @@ export default function HeroSection({
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="pt-10 animate-bounce">
         <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
         </svg>
